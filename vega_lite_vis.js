cfb6423ab91c149bacf7970e7cad69790ac8e9bb
tyzha9 <116022235+tyzha9@users.noreply.github.com>
2022-10-22 21:59:21 +1100

    Add files via upload

---
+++ vega_lite_vis.js
@@ -0,0 +1,28 @@
+var vg_1 = "json/choromap1.json";
+var vg_2 = "json/stackedbar2.json";
+var vg_3 = "json/area1.json";
+var vg_4 = "json/linegraph1.json";
+var vg_5 = "json/linegraph2.json";
+var vg_6 = "json/pie1.json";
+var vg_7 = "json/stackedbar1.json";
+vegaEmbed("#energy_map", vg_1).then(function (result) {
+
+}).catch(console.error);
+vegaEmbed("#ghg_bar", vg_2).then(function (result) {
+
+}).catch(console.error);
+vegaEmbed("#energy_area", vg_3).then(function (result) {
+
+}).catch(console.error);
+vegaEmbed("#us_line", vg_4).then(function (result) {
+
+}).catch(console.error);
+vegaEmbed("#russia_line", vg_5).then(function (result) {
+
+}).catch(console.error);
+vegaEmbed("#ghg_pie", vg_6).then(function (result) {
+
+}).catch(console.error);
+vegaEmbed("#energy_stacked", vg_7).then(function (result) {
+
+}).catch(console.error);
